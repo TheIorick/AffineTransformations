31285e85c73023854c47a0955a8565cd7cb17798
The_Iorick <kotolevskij.vlad@mail.ru>
2023-12-08 21:24:16 +0300

    add math libraries

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,9 @@
 package org.example;
 
+import org.example.math.vector.Vector2f;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Vector2f vector2f = new Vector2f(2.4F, 2.4F);
     }
 }
