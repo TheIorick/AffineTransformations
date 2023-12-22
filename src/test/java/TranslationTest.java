d5d8960f2d6a5050518e991022537db651d6ccb3
The_Iorick <kotolevskij.vlad@mail.ru>
2023-12-22 03:55:16 +0300

    realise test for Translation

--- src/test/java/TranslationTest.java
+++ src/test/java/TranslationTest.java
@@ -1,3 +1,24 @@
-public class TranslationTest {
+import org.example.affineTransform.scale.Scale;
+import org.example.affineTransform.translation.Translation;
+import org.example.math.vector.Vector3f;
+import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TranslationTest {
+    @Test
+    void doTranslateVectorOne(){
+        Vector3f vActual = new Vector3f(2f, 3f, 4f);
+        Translation translation = new Translation(1f, 2f, 3f);
+        Vector3f vExpected = translation.doTranslation(new Vector3f(1f, 1f, 1f));
+        assertEquals(vExpected, vActual);
+    }
+
+    @Test
+    void doTranslateCoeffOne(){
+        Vector3f vActual = new Vector3f(0, -1.5f, -2.7f);
+        Translation translation = new Translation(-1f, -2.5f, -3.7f);
+        Vector3f vExpected = translation.doTranslation(1f, 1f, 1f);
+        assertEquals(vExpected, vActual);
+    }
 }
